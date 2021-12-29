c5b325651d8b8195e5eccd466d17184676651234
maneesha2 <50171012+maneesha2@users.noreply.github.com>
2021-12-29 17:37:39 +0545

    minor changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function myFunction() {
-  var x = document.getElementById("categories");
-  if (x.className === "gamenav") {
-    x.className += " responsive";
-  } else {
-    x.className = "gamenav";
-  }
+var menu = document.getElementById("categories");
+var btn = menu.getElementsByClassName("categoriesitem");
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    var current = menu.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
