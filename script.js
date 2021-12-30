1055ccde71e0e44fee6adbe51eddd76516e03521
maneesha2 <50171012+maneesha2@users.noreply.github.com>
2021-12-30 13:14:26 +0545

    updates

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let categoryItems = document.getElementsByClassName("categoriesItem");
-for (let i = 0; i < categoryItems.length; i++) {
-  categoryItems[i].style.backgroundColor = "#f2f6ff";
-  categoryItems[i].style.color = "black";
-}
-categoryItems[n].style.backgroundColor = "#3366cc";
-categoryItems[n].style.color = "#ffffff";
+function onGameCategoryItemClick(n, gametype) {
+  let gt = gametype;
+  let gameItems = document.getElementsByClassName("gameItem");
+
+  for (let i = 0; i < gameItems.length; i++) {
+    let gameItemType = gameItems[i].getAttribute("data-game-type");
+
+    if (gameItemType === gt) {
+      gameItems[i].style.display = "block";
+    } else {
+      gameItems[i].style.display = "none";
+      // document.getElementById("gameComingSoon").style.display = "block";
+    }
+  }
+
+  let categoryItems = document.getElementsByClassName("categoriesItem");
+  for (let i = 0; i < categoryItems.length; i++) {
+    categoryItems[i].style.backgroundColor = "#f2f6ff";
+    categoryItems[i].style.color = "black";
+  }
+  categoryItems[n].style.backgroundColor = "#3366cc";
+  categoryItems[n].style.color = "#ffffff";
 }
 
 function onCarouselDotClick(n) {
@@ -48,5 +63,26 @@ function imageTransitionSlides() {
   setTimeout(imageTransitionSlides, 2000);
 }
 
-imageTransitionSlides();
+function testimonialTransitionSlides() {
+  let slides = document.getElementsByClassName("testimonialCarousel");
+  let dots = document.getElementsByClassName("testimoniaCarouselDot");
 
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].style.backgroundColor = "#f2f6ff";
+  }
+
+  testimonialSlideIndex++;
+  if (testimonialSlideIndex > slides.length) {
+    testimonialSlideIndex = 1;
+  }
+  slides[testimonialSlideIndex - 1].style.display = "block";
+  dots[testimonialSlideIndex - 1].style.backgroundColor = "blue";
+  setTimeout(testimonialTransitionSlides, 2000);
+}
+
+imageTransitionSlides();
+testimonialTransitionSlides();
